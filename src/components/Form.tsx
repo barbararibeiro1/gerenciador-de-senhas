ad704b43836867dc9626faaebf371419c649a16a
Barbara Ribeiro <barbararibeiro95@outlook.com>
2024-01-27 18:13:48 -0300

    feat: display para validação de senha

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,6 +14,23 @@ function Form({ handleViewData }: PropsForm) {
   });
 
   const senhaRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\-]).+/;
+  const verifyName = formInfo.name.length > 0;
+  const verifyLogin = formInfo.login.length > 0;
+  const verifySenha = formInfo.senha.length >= 8 && formInfo.senha.length < 16;
+  const regex1 = senhaRegex.test(formInfo.senha);
+
+  const verifyData = [
+    verifyName,
+    verifyLogin,
+    verifySenha,
+    regex1,
+  ];
+
+  console.log(verifyData);
+
+  const avaiable: string = 'valid-password-check';
+  const unavaiable: string = 'invalid-password-check';
+
   const verifyPassword = (formInfo.senha.length > 7
     && formInfo.senha.length < 17 && senhaRegex.test(formInfo.senha));
   const btnDisable = (formInfo.name !== '' && formInfo.login !== '' && verifyPassword);
@@ -24,7 +41,32 @@ function Form({ handleViewData }: PropsForm) {
       ...formInfo,
       [name]: value,
     });
+    handleEightCharactersState();
+    handleSixteenCharactersState();
+    handleLettersNumbersState();
+    handleAllCharactersState();
   };
+
+  const [eightCharacters, setEightCharacters] = useState<string>(unavaiable);
+  const [sixteenCharacters, setSixteenCharacters] = useState<string>(unavaiable);
+  const [lettersNumbers, setLettersNumbers] = useState<string>(unavaiable);
+  const [allCharacters, setAllCharacters] = useState<string>(unavaiable);
+
+  function handleEightCharactersState() {
+    setEightCharacters(verifySenha ? avaiable : unavaiable);
+  }
+
+  function handleSixteenCharactersState() {
+    setSixteenCharacters(regex1 ? avaiable : unavaiable);
+  }
+
+  function handleLettersNumbersState() {
+    setLettersNumbers(regex1 ? avaiable : unavaiable);
+  }
+
+  function handleAllCharactersState() {
+    setAllCharacters(regex1 ? avaiable : unavaiable);
+  }
 
   return (
     <form>
@@ -47,6 +89,14 @@ function Form({ handleViewData }: PropsForm) {
         id="senha"
         onChange={ handleChange }
       />
+      <div>
+        <p className={ `fistCheck ${eightCharacters}` }>Possuir 8 ou mais caracteres</p>
+        <p className={ `secondCheck ${sixteenCharacters}` }>Possuir até 16 caracteres</p>
+        <p className={ `thirdCheck ${lettersNumbers}` }>Possuir letras e números</p>
+        <p className={ `maisUmCheck ${allCharacters}` }>
+          Possuir algum caractere especial
+        </p>
+      </div>
       <InputLabel
         name="url"
         label="URL"
